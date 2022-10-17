82139fdcc3dce8bd3742374f8f04a6ff38f721ff
Alberto Muñoz <alberto.mqo@gmail.com>
2022-10-17 18:34:23 +0200

    Added status update bar

--- FacebookCompose/app/src/main/java/com/amqo/facebookcompose/presentation/composables/HomeScreen.kt
+++ FacebookCompose/app/src/main/java/com/amqo/facebookcompose/presentation/composables/HomeScreen.kt
@@ -1,25 +1,13 @@
 package com.amqo.facebookcompose.presentation.composables
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.amqo.facebookcompose.R
 import com.amqo.facebookcompose.ui.theme.FacebookComposeTheme
 
 @Composable
@@ -51,60 +39,7 @@ fun HomeScreen() {
     }
 }
 
-@Composable
-fun StatusUpdateBar(
-    avatarUrl: String,
-    onTextChange: (String) -> Unit,
-    onSendAction: () -> Unit
-) {
-    Surface {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 8.dp,
-                    vertical = 12.dp
-                ),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(avatarUrl)
-                    .crossfade(true)
-                    .placeholder(R.drawable.ic_placeholder)
-                    .build(),
-                contentDescription = stringResource(R.string.profile_image),
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-            )
-            var text by remember {
-                mutableStateOf("")
-            }
-            TextField(
-                colors = TextFieldDefaults.textFieldColors(
-                    backgroundColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                ),
-                modifier = Modifier.fillMaxWidth(),
-                value = text,
-                singleLine = true,
-                onValueChange = {
-                    text = it
-                    onTextChange(it)
-                },
-                placeholder = {
-                    Text(text = stringResource(id = R.string.what_is_on_your_mind))
-                },
-                keyboardActions = KeyboardActions(
-                    onSend = { onSendAction() }
-                ),
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send)
-            )
-        }
-    }
-}
+
 
 @Preview(widthDp = 400, heightDp = 300)
 @Composable
---
+++ FacebookCompose/app/src/main/java/com/amqo/facebookcompose/presentation/composables/StatusUpdateBar.kt
@@ -0,0 +1,151 @@
+package com.amqo.facebookcompose.presentation.composables
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.VideoCall
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.amqo.facebookcompose.R
+
+@Composable
+fun StatusUpdateBar(
+    avatarUrl: String,
+    onTextChange: (String) -> Unit,
+    onSendAction: () -> Unit
+) {
+    Surface {
+        Column {
+            StatusUpdateHeader(avatarUrl, onTextChange, onSendAction)
+            Divider()
+            StatusActionButtons()
+        }
+    }
+}
+
+@Composable
+private fun StatusActionButtons() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        StatusActionButton(
+            modifier = Modifier.weight(1f),
+            icon = Icons.Default.VideoCall,
+            text = stringResource(R.string.live_status_button),
+            onClick = {
+                // TODO
+            }
+        )
+        StatusActionButton(
+            modifier = Modifier.weight(1f),
+            icon = Icons.Default.Photo,
+            text = stringResource(R.string.photo_status_button),
+            onClick = {
+                // TODO
+            }
+        )
+        StatusActionButton(
+            modifier = Modifier.weight(1f),
+            icon = Icons.Default.Chat,
+            text = stringResource(R.string.discuss_status_button),
+            onClick = {
+                // TODO
+            }
+        )
+    }
+}
+
+@Composable
+fun StatusActionButton(
+    icon: ImageVector,
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = MaterialTheme.colors.onSurface,
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = icon, contentDescription = "")
+            Spacer(modifier = Modifier.width(6.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Composable
+private fun StatusUpdateHeader(
+    avatarUrl: String,
+    onTextChange: (String) -> Unit,
+    onSendAction: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 8.dp,
+                vertical = 12.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(avatarUrl)
+                .crossfade(true)
+                .placeholder(R.drawable.ic_placeholder)
+                .build(),
+            contentDescription = stringResource(R.string.profile_image),
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        var text by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            singleLine = true,
+            onValueChange = {
+                text = it
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.what_is_on_your_mind))
+            },
+            keyboardActions = KeyboardActions(
+                onSend = {
+                    if (text.isNotEmpty()) {
+                        onSendAction()
+                        text = ""
+                    }
+                }
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send)
+        )
+    }
+}
